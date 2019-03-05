0af98e4158865be34282ff0d82cbfc6c180b6ba2
Taylor Swinton <taylorswinton1@gmail.com>
2019-03-05 23:44:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
 
-#WHAT TRANSFER SHOULD BE ALLOWED TO DO  
+#WHAT TRANSFER SHOULD BE ALLOWED TO DO
 #Transfer should start in a pending status
 #Then go to complete state OR rejected status if the accounts aren't valid OR no money
 #Once completed it can be put into a reversed status.
