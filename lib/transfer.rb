e8c3fac5d1dfbc164e10404a0f6d824f1cf4269e
Taylor Swinton <taylorswinton1@gmail.com>
2019-03-06 00:03:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,6 +8,9 @@ class Transfer
   attr_accessor :sender, :receiver, :amount, :status
 
   def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
     @status = "pending"
   end
 
