24154c9501f8f33c05b85aa1f80005e40322f6cc
Taylor Swinton <taylorswinton1@gmail.com>
2019-03-06 00:01:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -5,8 +5,8 @@ class Transfer
 #Then go to complete state OR rejected status if the accounts aren't valid OR no money
 #Once completed it can be put into a reversed status.
 
-  def initialize
-
+  def initialize(sender, receiver, amount)
+    @status = "pending"
   end
 
 end
